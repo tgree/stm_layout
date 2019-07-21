dd7b34daee48ba53ff0992ce16bafd31d04a4de1
Terry Greeniaus <terrygreeniaus@gmail.com>
2019-07-20 23:17:42 -0600

    wip

--- chip_db.py
+++ chip_db.py
@@ -69,3 +69,98 @@ def make_package(dev):
         dim = int(math.ceil(float(n)/4.))
         return chip_package.LQFP(dim, dim)
     raise KeyError
+
+
+GPIO_DEFAULTS = {
+    'RM0433' : {
+        'MODER' : {
+            'PA' : 0xABFFFFFF,
+            'PB' : 0xFFFFFEBF,
+            'PC' : 0xFFFFFFFF,
+            'PD' : 0xFFFFFFFF,
+            'PE' : 0xFFFFFFFF,
+            'PF' : 0xFFFFFFFF,
+            'PG' : 0xFFFFFFFF,
+            'PH' : 0xFFFFFFFF,
+            'PI' : 0xFFFFFFFF,
+            'PJ' : 0xFFFFFFFF,
+            'PK' : 0xFFFFFFFF,
+        },
+        'OTYPER' : {
+            'PA' : 0x00000000,
+            'PB' : 0x00000000,
+            'PC' : 0x00000000,
+            'PD' : 0x00000000,
+            'PE' : 0x00000000,
+            'PF' : 0x00000000,
+            'PG' : 0x00000000,
+            'PH' : 0x00000000,
+            'PI' : 0x00000000,
+            'PJ' : 0x00000000,
+            'PK' : 0x00000000,
+        },
+        'OSPEEDR' : {
+            'PA' : 0x0C000000,
+            'PB' : 0x000000C0,
+            'PC' : 0x00000000,
+            'PD' : 0x00000000,
+            'PE' : 0x00000000,
+            'PF' : 0x00000000,
+            'PG' : 0x00000000,
+            'PH' : 0x00000000,
+            'PI' : 0x00000000,
+            'PJ' : 0x00000000,
+            'PK' : 0x00000000,
+        },
+        'PUPDR' : {
+            'PA' : 0x64000000,
+            'PB' : 0x00000100,
+            'PC' : 0x00000000,
+            'PD' : 0x00000000,
+            'PE' : 0x00000000,
+            'PF' : 0x00000000,
+            'PG' : 0x00000000,
+            'PH' : 0x00000000,
+            'PI' : 0x00000000,
+            'PJ' : 0x00000000,
+            'PK' : 0x00000000,
+        },
+        'AFR' : {
+            'PA' : 0x0000000000000000,
+            'PB' : 0x0000000000000000,
+            'PC' : 0x0000000000000000,
+            'PD' : 0x0000000000000000,
+            'PE' : 0x0000000000000000,
+            'PF' : 0x0000000000000000,
+            'PG' : 0x0000000000000000,
+            'PH' : 0x0000000000000000,
+            'PI' : 0x0000000000000000,
+            'PJ' : 0x0000000000000000,
+            'PK' : 0x0000000000000000,
+        },
+    },
+}
+
+REFM_TABLE = {
+    '^stm32h742.*' : 'RM0433',
+    '^stm32h743.*' : 'RM0433',
+    '^stm32h753.*' : 'RM0433',
+    '^stm32h750.*' : 'RM0433',
+}
+
+def get_gpio_defaults(identifier, port, pin):
+    refm = None
+    for k, v in REFM_TABLE.items():
+        regex = re.compile(k)
+        if regex.search(identifier):
+            refm = v
+            break
+    if not refm:
+        raise KeyError
+
+    defs    = GPIO_DEFAULTS[refm]
+    moder   = defs['MODER'][port]
+    otyper  = defs['OTYPER'][port]
+    ospeedr = defs['OSPEEDR'][port]
+    pupdr   = defs['PUPDR'][port]
+    afr     = defs['AFR'][port]
